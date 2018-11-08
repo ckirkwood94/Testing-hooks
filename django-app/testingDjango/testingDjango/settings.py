2723e530b9ee098160efe3e8e1f95dcf9b3ac074
ckirkwood94 <ckirkwood94@gmail.com>
2018-11-08 12:18:12 -0500

    Environ var for secret_key

--- django-app/testingDjango/testingDjango/settings.py
+++ django-app/testingDjango/testingDjango/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -88,8 +88,6 @@ DATABASES = {
         'PORT': DATABASE_PORT,
     }
 }
-
-print(DATABASE_ENGINE)
 
 
 # Password validation
