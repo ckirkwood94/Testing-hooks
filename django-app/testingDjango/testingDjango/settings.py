e59a0a1a3c778ab72a957d5291e4595548f0878b
ckirkwood94 <ckirkwood94@gmail.com>
2018-11-08 10:56:55 -0500

    Edit comment section

--- django-app/testingDjango/testingDjango/settings.py
+++ django-app/testingDjango/testingDjango/settings.py
@@ -130,7 +130,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Imports settings from settings_local.py and overwrites vars in this file. settings_local.py is required to run app. Follow instructions in settings_local.template
+# Imports settings from settings_local.py and overwrites vars in this file.
+# settings_local.py is required to run app. Follow instructions in settings_local.template
 try:
     from settings_local import *
 except ImportError:
